d130a3f93291b66c2e8b4d25ebfa853dad3d7fe7
tcnickless <tcnickless@gmail.com>
2025-09-02 21:18:57 +0100

    added cli arguments for chat query

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 def main():
-    print("Hello from womblex!")
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    user_query = sys.argv[1]
+
+    print("=== Hello from womblex! ===")
+    print("===========================")
+    response = client.models.generate_content(model="gemini-2.0-flash-001", contents=user_query)
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
